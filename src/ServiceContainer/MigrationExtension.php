e935655ddebe197fd04c8f4cd3183edddfd2361d
George Tarkalanov <gtarkalanov@propeople.dk>
2017-05-22 18:01:19 +0300

    Extension file

---
+++ src/ServiceContainer/MigrationExtension.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @author George Tarkalanov, <g.turkalanov@gmail.com>
+ */
+namespace gtarkalanov\MigrationExtension\ServiceContainer;
+
+use Behat\EnvironmentLoader;
+use Behat\Testwork\ServiceContainer\Extension;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+/**
+ * Class MigrationExtension.
+ *
+ * @package Behat\MigrationExtension\ServiceContainer
+ */
+class MigrationExtension implements Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigKey()
+    {
+        return 'migration';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(ExtensionManager $extensionManager)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ContainerBuilder $container, array $config)
+    {
+        $loader = new EnvironmentLoader($this, $container, $config);
+        $loader->load();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        $config = $builder->children();
+
+        foreach (['database_name', 'database_username', 'database_password'] as $param) {
+            $config->scalarNode($param)
+                ->end();
+        }
+
+        $config->end();
+    }
+}
